34ffdaff2199e1b06e4d9a69c114333b8cd3c9b6
Ramesh <gnvkdramesh@gmail.com>
2026-04-05 06:21:48 +0530

    Added this python script as part of excercise

--- 01-introduction/Excercise/hello_world.py
+++ 01-introduction/Excercise/hello_world.py
@@ -2,25 +2,25 @@
 Lesson 01 - Hello World with LangChain
 This example demonstrates a basic LLM call using ChatOpenAI.
 """
+
 import os
 
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 
-
-
 load_dotenv()
 
 def main():
     print("🦜🔗 Hello LangChain!\n")
+
+    # Create a chat model instance
     model = ChatOpenAI(
         model=os.getenv("AI_MODEL"),
         base_url=os.getenv("AI_ENDPOINT"),
         api_key=os.getenv("AI_API_KEY")
-
     )
     response = model.invoke("Say 'Hello, Ramesh'")
     print("AI Response:" , response.content)
 
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
