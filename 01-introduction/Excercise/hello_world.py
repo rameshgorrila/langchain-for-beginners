90d293b1b17f48b8197f93624990e05c9a8bda70
Ramesh <gnvkdramesh@gmail.com>
2026-04-05 06:20:43 +0530

    Changed alignment

---
+++ 01-introduction/Excercise/hello_world.py
@@ -0,0 +1,26 @@
+"""
+Lesson 01 - Hello World with LangChain
+This example demonstrates a basic LLM call using ChatOpenAI.
+"""
+import os
+
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+
+
+load_dotenv()
+
+def main():
+    print("🦜🔗 Hello LangChain!\n")
+    model = ChatOpenAI(
+        model=os.getenv("AI_MODEL"),
+        base_url=os.getenv("AI_ENDPOINT"),
+        api_key=os.getenv("AI_API_KEY")
+
+    )
+    response = model.invoke("Say 'Hello, Ramesh'")
+    print("AI Response:" , response.content)
+
+    if __name__ == "__main__":
+        main()
